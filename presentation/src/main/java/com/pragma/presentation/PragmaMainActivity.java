c94c69b4ba093966a39f5c21188290db4a134169
Anbu Ezhilan <anbu.ezhilan@massive.co>
2017-03-07 12:42:42 +1100

    Added Log Statements

--- presentation/src/main/java/com/pragma/presentation/PragmaMainActivity.java
+++ presentation/src/main/java/com/pragma/presentation/PragmaMainActivity.java
@@ -2,12 +2,17 @@ package com.pragma.presentation;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class PragmaMainActivity extends AppCompatActivity {
+
+    private static final String TAG = PragmaMainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
         setContentView(R.layout.activity_pragma_main);
+
     }
 }
