5b591cae61b8d5d8aa138d64da98b50873f72fb2
Anbu Ezhilan <anbu.ezhilan@massive.co>
2017-03-07 12:36:26 +1100

    Initial commit

---
+++ presentation/src/main/java/com/pragma/presentation/PragmaMainActivity.java
@@ -0,0 +1,13 @@
+package com.pragma.presentation;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class PragmaMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pragma_main);
+    }
+}
